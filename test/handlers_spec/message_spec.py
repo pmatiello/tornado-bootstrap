d2b35d04e65c3c28c669e18bcb876036f3f045a8
Pedro Matiello <pmatiello@gmail.com>
2010-09-17 16:41:31 -0300

    Added tests for message_handler.

--- test/handlers_spec/message_spec.py
+++ test/handlers_spec/message_spec.py
@@ -1,5 +1,6 @@
 from mockito import any, mock, verify, when
-from handlers.message import messages_handler
+from handlers.message import message_handler, messages_handler
+from models.message import message
 
 class messages_handler_spec():
     
@@ -17,9 +18,31 @@ class messages_handler_spec():
         verify(self.handler).render('index.html', messages=self.message_list)
     
     def should_create_new_messages(self):
-        when(self.handler).param('name').thenReturn('Author name')
-        when(self.handler).param('message').thenReturn('Message text')
+        when(self.handler).param('name').thenReturn("Author name")
+        when(self.handler).param('message').thenReturn("Message text")
         self.handler.post()
-        verify(self.repository).list()
-        verify(self.repository).save(any())
-        verify(self.handler).render('index.html', messages=self.message_list)
+        verify(self.repository).save(any(message))
+
+class message_handler_spec():
+    
+    def setup(self):
+        self.handler = object.__new__(message_handler)
+        self.repository = mock()
+        self.handler.initialize(self.repository)
+        self.message = message("Author name", "Message text")
+        when(self.repository).load(1).thenReturn(self.message)
+        when(self.handler).render().thenReturn(None)
+    
+    def should_modify_messages(self):
+        when(self.handler).param('name').thenReturn("Author name updated")
+        when(self.handler).param('message').thenReturn("Message text updated")
+        self.handler.put(1)
+        assert self.message.name == "Author name updated"
+        assert self.message.message == "Message text updated"
+        verify(self.repository).save(self.message)
+    
+    def should_delete_messages(self):
+        when(self.handler).param('name').thenReturn("Author name updated")
+        when(self.handler).param('message').thenReturn("Message text updated")
+        self.handler.delete(1)
+        verify(self.repository).remove(self.message)
