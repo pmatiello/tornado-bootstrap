b0c320655e11bccae6427ac45e778656085c6cfe
Pedro Matiello <pmatiello@gmail.com>
2010-09-17 20:38:37 -0300

    Renamed a few tests.

--- test/specs/models/entity_spec.py
+++ test/specs/models/entity_spec.py
@@ -11,7 +11,7 @@ class entity_repository_spec():
         self.repository = entity_repository(self.session)
         self.msg = entity("Field1 data", "Field1 data")
     
-    def should_load_entities(self):
+    def should_load_instances(self):
         query = mock()
         filtered_query = mock()
         when(self.session).query(entity).thenReturn(query)
@@ -19,17 +19,17 @@ class entity_repository_spec():
         when(filtered_query).first().thenReturn(self.msg)
         assert self.repository.load(1) == self.msg
     
-    def should_save_entities(self):
+    def should_save_instances(self):
         self.repository.save(self.msg)
         verify(self.session).add(self.msg)
         verify(self.session).commit()
     
-    def should_delete_entities(self):
+    def should_delete_instances(self):
         self.repository.remove(self.msg)
         verify(self.session).delete(self.msg)
         verify(self.session).commit()
     
-    def should_list_entitities(self):
+    def should_list_instances(self):
         query = mock()
         when(self.session).query(entity).thenReturn(query)
         when(query).all().thenReturn([self.msg])
